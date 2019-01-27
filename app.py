de7eaf1b66afe01899aa60047a3432ec06d73c89
aboud20-meet <aboud20@meet.mit.edu>
2019-01-27 19:43:42 +0200

    a

--- app.py
+++ app.py
@@ -24,33 +24,9 @@ def mmorpg():
 def moba():
 	return render_template ("moba.html")
 
-@app.route('/' , methods = ['GET','POST'])
-def login_page():
-	
-    if request.method == 'POST':
-        user = databases.query_by_name(request.form["name"])
-        print(request.form['name'])
-        if user is not None:
-            if user.password == request.form["password"]:
-                session['username'] = user.name
-                return redirect(url_for("homepage.html"))
-
-            else:
-                error = 'password does not match'
-                return render_template('homepage.html', error = error)
-        else:
-
-            error = 'username does not exist'
-            return render_template('homepage.html', error = error)
-
-    else:
-
-        return render_template('homepage.html')
-
 @app.route('/signup')
 def signup():
     return render_template('signup.html')
-
 
 
 if __name__ == '__main__':
